4fad33552964d34c424ed789c5925ea23af524b8
Daniel <gendik38@gmail.com>
2023-05-09 18:37:33 +0200

    add hover animation

--- src/components/Navbar/components/DownloadBtn/DownloadCV.tsx
+++ src/components/Navbar/components/DownloadBtn/DownloadCV.tsx
@@ -4,7 +4,7 @@ export const DownloadCV = () => {
   return (
     <>
       <button className=" border-2 border-black rounded-xl
-       px-4 py-3 font-bold flex items-center gap-2 ">
+       px-4 py-3 font-bold flex items-center gap-2 hover:bg-black hover:text-white duration-150">
         {" "}
         Download CV
         <BsDownload />
