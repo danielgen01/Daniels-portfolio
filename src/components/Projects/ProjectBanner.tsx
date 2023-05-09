ac7ffec78aa7406c25b39f784ca0a173a7c15a20
Daniel <gendik38@gmail.com>
2023-05-09 18:01:14 +0200

    add corect links

--- src/components/Projects/ProjectBanner.tsx
+++ src/components/Projects/ProjectBanner.tsx
@@ -5,10 +5,6 @@ import ECommerceScreen from "/Assets/projects/E-Commerce-Screen.webp"
 import rockpapergame from "/Assets/projects/rock_paper_scissors_lizzard_spock.png"
 import starbuckscreen from "/Assets/projects/starbucks_screen_2.webp"
 
-
-
-
-
 const ProjectBanner = () => {
   return (
     <section className="min-h-[500px] bg-medium-blue flex flex-col gap-4">
@@ -26,14 +22,58 @@ const ProjectBanner = () => {
           skills in action.
         </p>
 
-        <div className="projects-list grid grid-cols-1 md:grid-cols-2 gap-20 mt-5">
-          <Project image={KanbanScreen}/>
-          <Project image={InvoiceScreen}/>
-          <Project image={ECommerceScreen}/>
-          <Project image={rockpapergame}/>
-          <Project image={starbuckscreen}/>
-
-
+        <div className="projects-list grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
+          <Project
+            title={"Kanban Task Management Business App"}
+            description={
+              "I built an Kanbanboard from scratch.This board is also connected to localstorage to save your changes. I used React with Typescript to build components and Redux for state management. I styled my app with TailwindCSS."
+            }
+            githubLink={"https://github.com/danielgen01/kanbanboard-frontend"}
+            liveViewLink={"https://kanban-project-by-daniel-g.netlify.app/"}
+            image={KanbanScreen}
+          />
+          <Project
+            title={"Invoice Business App"}
+            description={
+              "I built an business App where you can manage client invoices. I styled my app with TailwindCSS and used React to create components."
+            }
+            githubLink={"https://github.com/danielgen01/invoice-app"}
+            liveViewLink={"https://invoice-app-by-daniel-g.netlify.app/"}
+            image={InvoiceScreen}
+          />
+          <Project
+            title={"E-Commerce App Multipage"}
+            description={
+              "I built an E-Commerce store from scratch. I used React with Typescript to build components and Redux for state management. I styled my app with TailwindCSS."
+            }
+            githubLink={
+              "https://github.com/danielgen01/E-Commerce-with-React-Redux"
+            }
+            liveViewLink={"https://e-commercestore-react-redux.netlify.app/"}
+            image={ECommerceScreen}
+          />
+          <Project
+            title={"Rock - Paper - Scissors - Lizard - Spock Game minimal"}
+            description={
+              "I built the extended version of the rock paper scissors game. I developed this project with React,TypeScript and TailwindCSS.            "
+            }
+            githubLink={
+              "https://github.com/danielgen01/rock-paper-scissors-game"
+            }
+            liveViewLink={
+              "https://rock-paper-scissors-by-daniel-g.netlify.app/"
+            }
+            image={rockpapergame}
+          />
+          <Project
+            title={"Starbucks Clone landing page (static)"}
+            description={
+              "I built an clone of the german starbucks landing page to prove that I'm able to develop prefebrecated designs. I used React with TypeScript to create components and TailwindCSS for the styling."
+            }
+            githubLink={"https://github.com/danielgen01/starbucksclone"}
+            liveViewLink={"https://clone-starbucks-daniel-gen.netlify.app/"}
+            image={starbuckscreen}
+          />
         </div>
       </div>
     </section>
@@ -41,5 +81,3 @@ const ProjectBanner = () => {
 }
 
 export default ProjectBanner
-
-
--- src/components/Projects/ProjectComp.tsx
+++ src/components/Projects/ProjectComp.tsx
@@ -1,14 +1,54 @@
+import { SiReact, SiTypescript, SiRedux, SiTailwindcss } from "react-icons/si"
+import { BsArrowUpRight, BsGithub } from "react-icons/bs"
 type ProjectProps = {
-  image: any
+  image: string
+  title:string
+  description:string
+  liveViewLink:string
+  githubLink:string
 }
-export const Project: React.FC<ProjectProps> = ({ image }) => {
+export const Project: React.FC<ProjectProps> = ({ image, title,description,githubLink,liveViewLink }) => {
   return (
-    <div className="w-full  max-h-[250px]  ring-8 ring-white rounded-md">
-      <img
-        src={image}
-        alt="Project_screen"
-        className="w-full h-full hover:opacity-50"
-      />
+    <div
+      className="grid-layout grid grid-cols-1 lg:grid-cols-2 mx-10 mt-10 bg-gray-500/10 rounded-md p-5
+    ring-4 ring-white 
+    "
+    >
+      <div className="grid-item-image-container flex flex-col gap-10 order-2 lg:order-1">
+        <img src={image} alt="E-Commerce-Screenshot" />
+        <div className="used-technologies-list flex flex-row gap-10 text-4xl lg:text-6xl">
+          <SiReact className="text-[#61DBFB]" />
+          <SiTypescript className=" text-[#007acc]" />
+          <SiRedux className="text-purple-700" />
+          <SiTailwindcss className="text-sky-500" />
+        </div>
+        <div className="buttons grid grid-cols-1 lg:grid-cols-2 gap-5">
+          <a
+            href={liveViewLink}
+            target={"_blank"}
+            className="bg-gradient-to-r from-sky-500 to-indigo-500 px-2 py-2 rounded-md font-medium hover:opacity-75 flex items-center gap-2 w-full justify-center text-white"
+          >
+            View Livedemo
+            <BsArrowUpRight />
+          </a>
+          <a
+            href={githubLink}
+            className="bg-white px-2 py-2 rounded-md font-medium hover:opacity-75 flex items-center justify-center gap-2 w-full"
+            target={"_blank"}
+          >
+            <BsGithub className="h-[20px] w-fit text-[#171515] max-w-fit" />
+          </a>
+        </div>
+      </div>
+      <div className="project-description text-white flex flex-col items-center gap-5 justify-between">
+        <h1 className="font-semibold text-xl text-center mt-2">
+        {title}
+        </h1>
+
+        <h2 className="lg:text-2xl text-md lg:mx-20">
+         {description}
+        </h2>
+      </div>
     </div>
   )
 }
