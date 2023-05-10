ffe673eea0a845190a35e4bd5ae20735cf5fd104
Daniel <gendik38@gmail.com>
2023-05-10 08:44:16 +0200

    make section responsive

--- src/components/TechStack/TechStack.tsx
+++ src/components/TechStack/TechStack.tsx
@@ -4,24 +4,34 @@ import waves from "/Assets/Vektor_waves.png"
 const TechStack = () => {
   return (
     <section className=" bg-bright-gray w-full overflow-x-hidden">
-      <div className="techstack-content py-5  px-10 flex flex-col gap-4">
+      <div className="techstack-content py-5  px-5 flex flex-col gap-4">
         <img src={waves} alt="waves_icon" className=" ml-auto" />
-        <div className="headline flex items-center">
-          <h1 className="text-4xl font-bold ">
-            Tech Stack + <br /> technologies
-          </h1>
-          <div className="border-b-4 border-black  mt-20 ml-5 w-3/4"></div>
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+          <div className="grid-1 flex flex-col gap-5 lg:order-2 lg:items-start">
+            <h1 className="text-4xl font-bold ">
+              Tech Stack + <br /> technologies
+            </h1>
+            {/* <div className="border-b-4 border-black  mt-20 ml-5 w-3/4"></div> */}
+            <p className="font-semibold text-left">
+              As a dedicated web developer, I'm skilled in HTML5, CSS3,
+              JavaScript, and TypeScript, enabling me to build intuitive,
+              robust, and responsive web applications. I'm proficient in React
+              for building fast, scalable SPAs, and Tailwind for efficient,
+              custom designs. I use Git and GitHub for version control and team
+              collaboration, and leverage ChatGPT to enhance work efficiency
+              through automation. Committed to innovation, I constantly refine
+              my skills to meet client demands.
+            </p>
+          </div>
+
+          <div className="image grid-2">
+            <img
+              src={techstackImage}
+              alt="Daniel's Techstack"
+              className="w-full"
+            />
+          </div>
         </div>
-        <p className="font-semibold ">
-          As a dedicated web developer, I'm skilled in HTML5, CSS3, JavaScript,
-          and TypeScript, enabling me to build intuitive, robust, and responsive
-          web applications. I'm proficient in React for building fast, scalable
-          SPAs, and Tailwind for efficient, custom designs. I use Git and GitHub
-          for version control and team collaboration, and leverage ChatGPT to
-          enhance work efficiency through automation. Committed to innovation, I
-          constantly refine my skills to meet client demands.
-        </p>
-        <img src={techstackImage} alt="Daniel's Techstack" />
       </div>
     </section>
   )
--- src/components/TopIntroduction/Introduction.tsx
+++ src/components/TopIntroduction/Introduction.tsx
@@ -4,38 +4,44 @@ import portrait from "/Assets/BewerbungsbildCartoon.webp"
 
 const Introduction = () => {
   return (
-    <section className="Introdocution py-10 px-20 flex flex-col gap-4 ">
-      <div className="headline flex items-center">
-        <h1 className="text-4xl font-bold whitespace-nowrap">Hi, I'm Daniel</h1>
-        <span role="img" aria-label="waving hand" className="text-4xl">
-          👋
-        </span>
-      </div>
+    <section className="Introdocution py-10 px-20 grid grid-cols-1 gap-4 lg:grid-cols-2">
+      <section className="first-grid flex flex-col gap-5">
+        <div className="headline flex items-center">
+          <h1 className="text-4xl font-bold whitespace-nowrap">
+            Hi, I'm Daniel
+          </h1>
+          <span role="img" aria-label="waving hand" className="text-4xl">
+            👋
+          </span>
+        </div>
 
-      <div className="frontend-dev flex items-center gap-5">
-        <div className="border-b-4 border-black w-1/4 "></div>
-        <h2 className="font-bold text-lg">Frontend developer</h2>
-      </div>
-      <p className="font-bold text-left">
-        Hello, I'm Daniel Gendik, a passionate 22-year-old Frontend Web
-        Developer hailing from Germany. Scroll down to explore my projects and
-        connect with me on social media.
-      </p>
-      <div className="button download-cv flex justify-center">
-        <button
-          className="bg-gradient-to-r from-sky-500 to-indigo-500 font-bold text-white 
-      rounded-lg px-7 py-3 flex items-center gap-5 hover:opacity-80"
-        >
-          JavaScript Certificate
-          <BsDownload className="text-white font-bold" />
-        </button>
-      </div>
-      <div className="button-react-testing-certificate flex justify-center">
-        <button className="underline font-bold underline-offset-2 flex items-center gap-2 whitespace-nowrap">
-          React Testing Library Certificate
-          <BsDownload className="text-black font-bold" />
-        </button>
-      </div>
+        <div className="frontend-dev flex items-center gap-5">
+          <div className="border-b-4 border-black w-1/4 "></div>
+          <h2 className="font-bold text-lg">Frontend developer</h2>
+        </div>
+        <p className="font-bold text-left">
+          Hello, I'm Daniel Gendik, a passionate 22-year-old Frontend Web
+          Developer hailing from Germany. Scroll down to explore my projects and
+          connect with me on social media.
+        </p>
+        <div className="buttons flex flex-col lg:flex-row gap-5">
+          <button
+            className="bg-gradient-to-r from-sky-500 to-indigo-500 font-bold text-white 
+      rounded-lg px-7 py-3 flex items-center gap-5 hover:opacity-80 whitespace-nowrap"
+          >
+            JavaScript Certificate
+            <BsDownload className="text-white font-bold" />
+          </button>
+
+          <button className="underline font-bold underline-offset-2 flex items-center gap-2 whitespace-nowrap">
+            React Testing Library Certificate
+            <BsDownload className="text-black font-bold" />
+          </button>
+        </div>
+      </section>
+
+      <section className="second-grid"></section>
+
       {/* <figure
         className="portrait relative  flex justify-center items-start"
        
