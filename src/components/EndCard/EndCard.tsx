47cf679a8c99c34b743a1389a18cef948096f163
Daniel <gendik38@gmail.com>
2023-05-11 17:25:12 +0200

    add alert if someone trys to schedule a call

--- src/components/EndCard/EndCard.tsx
+++ src/components/EndCard/EndCard.tsx
@@ -11,30 +11,35 @@ const EndCard = () => {
             <h1 className="text-white font-bold text-center text-4xl lg:text-left">
               Let's build something together
             </h1>
-            <button className="bg-gradient-to-r from-sky-500 to-indigo-500 px-5 py-2 rounded-xl text-white font-semibold flex items-center gap-2 cursor-not-allowed">
+            <button
+              className="bg-gradient-to-r from-sky-500 to-indigo-500 px-5 py-2 rounded-xl text-white font-semibold flex items-center gap-2 cursor-not-allowed"
+              onClick={() => {
+                alert("Not avaible yet")
+              }}
+            >
               Schedule a free call
               <img src={calendarcheck} alt="calendar" />
             </button>
           </div>
           <div className="border-b-2 border-white/50 w-full lg:mt-5 lg:mb-5"></div>
           <footer className="flex justify-between lg:mt-5 gap-5 ">
-          <img src={logo} alt="Logo" />
+            <img src={logo} alt="Logo" />
 
-          <div className="socials flex items-center gap-4">
-            <p className="text-white text-left font-semibold">
-              Stay tuned in loop:
-            </p>
-            <a
-              href="https://www.linkedin.com/in/daniel-gendik-b7b277254/"
-              target="_blank"
-            >
-              <BsLinkedin className="text-4xl text-[#0A66C2]" />
-            </a>
+            <div className="socials flex items-center gap-4">
+              <p className="text-white text-left font-semibold">
+                Stay tuned in loop:
+              </p>
+              <a
+                href="https://www.linkedin.com/in/daniel-gendik-b7b277254/"
+                target="_blank"
+              >
+                <BsLinkedin className="text-4xl text-[#0A66C2]" />
+              </a>
 
-            <a href="https://github.com/danielgen01" target="_blank">
-              <BsGithub className="text-4xl text-[#000000]" />
-            </a>
-          </div>
+              <a href="https://github.com/danielgen01" target="_blank">
+                <BsGithub className="text-4xl text-[#000000]" />
+              </a>
+            </div>
           </footer>
         </div>
       </section>
