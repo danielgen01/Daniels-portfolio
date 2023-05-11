36db1e3ae1731bcc78ade95370aa4fa544d349fc
Daniel <gendik38@gmail.com>
2023-05-11 17:13:17 +0200

    remove overflow on x AXIS

--- src/components/Navbar/components/TouchMenu/TouchMenu.tsx
+++ src/components/Navbar/components/TouchMenu/TouchMenu.tsx
@@ -8,12 +8,12 @@ const TouchMenu: React.FC<TouchMenuProps> = ({ isMenuOpen, toggleMenu }) => {
   return (
     <>
       <div
-        className="bg-black/50 fixed top-30 left-0 w-screen h-screen cursor-pointer"
+        className="bg-black/50 fixed top-30 left-0 w-screen h-screen cursor-pointer overflow-x-hidden"
         style={{ display: isMenuOpen ? "block" : "none" }}
         onClick={toggleMenu}
       ></div>
       <div
-        className="Touch-menu w-full bg-dark-bg min-h-[200px] z-50 absolute"
+        className="Touch-menu w-full bg-dark-bg min-h-[200px] z-50 absolute overflow-x-hidden"
         style={{ display: isMenuOpen ? "block" : "none" }}
       >
         <div className="menu-content py-5 flex flex-col">
